f88cdf4d18e03959b4ccf68df4673bf040b0b90d
gnanamoorthy-g <gnanamoorthy@zumen.com>
2023-07-28 10:29:18 +0530

    sass added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import  { createRoot }  from 'react-dom/client';
 import App from './src/App.js'
+import './styles/scss/main.scss'
+import './styles/css/index.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
