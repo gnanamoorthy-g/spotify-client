d8ead0d40b24aefd3fdee1fdfe8285365a26d0b3
am-i-a-dream <ggnanamoorthy215@gmail.com>
2023-10-25 01:31:44 +0530

    preload bootstrap & fonts

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const DotEnv = require('dotenv-webpack')
 
 module.exports = {
     entry: './index.js',
-    mode: 'development',
-    devtool : 'inline-source-map',
-    // mode: 'production',
-    // devtool : 'source-map',
+    // mode: 'development',
+    // devtool : 'inline-source-map',
+    mode: 'production',
+    devtool : 'source-map',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
